eb36dd5c90a26efba2a8610fb483e8a7ad2d32e9
usherwong <wang.yin.shu@gmail.com>
2014-03-24 21:15:10 +0800

    greet test

---
+++ bin/greet.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+var greet = require("../index.js");
+var parseArgs = require("minimist");
+//console.log(greet(process.argv[2]));
+var agrst = parseArgs(process.argv.slice(2));
+//console.log(agrs._[0]);
+console.log(greet(agrst._[0],agrst.drunk));
---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = function (name,drunk){
+	if(drunk){
+		return "hello "+name +",you look sexy tody.";
+	}else{
+		return "hello "+name;
+	}
+}
